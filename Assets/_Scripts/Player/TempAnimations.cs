84a9beff55284bf188f6f9ad7b5f0d7fa8e99880
Era Soren <era.soren0@gmail.com>
2023-03-10 23:22:18 +0300

    Updated character animations.

---
+++ Assets/_Scripts/Player/TempAnimations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace EraSoren.Player
+{
+    public class TempAnimations : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+
+        [SerializeField] private KeyAnimationPair[] animations;
+        
+        [Serializable]
+        private struct KeyAnimationPair
+        {
+            public KeyCode keyCode;
+            public string animationName;
+        }
+
+        private void Update()
+        {
+            foreach (var animationPair in animations)
+            {
+                if (Input.GetKeyDown(animationPair.keyCode))
+                {
+                    animator.Play(animationPair.animationName);
+                }
+            }
+        }
+    }
+}
